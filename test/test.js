2f9af42146d7113e179066545c5f355114e25d60
danypype <danypype@gmail.com>
2014-05-08 15:05:38 -0500

    Added tests for comparison operators

--- test/test.js
+++ test/test.js
@@ -1,42 +1,74 @@
-var assert = require("assert")
-,   mquery = require("..")
+var assert     = require("assert")
+,   fs         = require("fs")
+,   mquery     = require("..")
 ,   Collection = mquery.Collection;
 
 describe("Collection", function () {
   var collection = new Collection();
 
   describe("#insert", function () {
-    describe("insert one entry", function () {
+    it("should insert the given entry", function () {
       collection.insert({a: 1, b: 2});
       assert.deepEqual(collection[0], {a: 1, b: 2});
     });
-    describe("insert multiple entries", function () {
-      describe("insert sending multiple arguments", function () {
-        collection.insert({a: 3, b: 4}, {a: 5, b: 6});
-        assert.deepEqual(collection[1], {a: 3, b: 4});
-        assert.deepEqual(collection[2], {a: 5, b: 6});
-      });
-      describe("insert sending an array", function () {
-        collection.insert([{a: 7, b: 8}, {a: 9, b: 10}]);
-        assert.deepEqual(collection[3], {a: 7, b: 8});
-        assert.deepEqual(collection[4], {a: 9, b: 10});
-      });
+    it("should insert all entries sent as arguments", function () {
+      collection.insert({a: 3, b: 4}, {a: 5, b: 6});
+      assert.deepEqual(collection[1], {a: 3, b: 4});
+      assert.deepEqual(collection[2], {a: 5, b: 6});
+    });
+    it("should insert all entries in the array", function () {
+      collection.insert([{a: 7, b: 8}, {a: 9, b: 10}]);
+      assert.deepEqual(collection[3], {a: 7, b: 8});
+      assert.deepEqual(collection[4], {a: 9, b: 10});
     });
   });
 
   describe("#removeEntries", function () {
-    describe("remove one entry", function () {
+    it("should remove the given entry", function () {
       collection.removeEntries(collection[0]);
       assert.deepEqual(collection[0], {a: 3, b: 4});
     });
-    describe("remove multiple entries", function () {
-      describe("remove sending multiple arguments", function () {
-        collection.removeEntries(collection[0], collection[1]);
-        assert.deepEqual(collection[0], {a: 7, b: 8});
+    it("should remove all entries sent as arguments", function () {
+      collection.removeEntries(collection[0], collection[1]);
+      assert.deepEqual(collection[0], {a: 7, b: 8});
+    });
+    it("should remove all entries in the array", function () {
+      collection.removeEntries([collection[0], collection[1]]);
+      assert.equal(collection.length, 0);
+    });
+  });
+
+  var users = new Collection(JSON.parse(fs.readFileSync("./test/source.json")));
+
+  describe("#find", function () {
+    describe("comparison operators", function () {
+      it("should return the entries matching {gender: {$eq: 'female'}}", function () {
+        var females = users.find({gender: {$eq: "female"}});
+        assert.equal(females.length, 5);
+        females.forEach(function (user) {
+          assert.equal(user.gender, "female");
+        });
       });
-      describe("remove sending an array", function (argument) {
-        collection.removeEntries([collection[0], collection[1]]);
-        assert.equal(collection.length, 0);
+      it("should return the entries matching {gender: {$ne: 'female'}}", function () {
+        var males = users.find({gender: {$ne: "female"}});
+        assert.equal(males.length, 2);
+        males.forEach(function (user) {
+          assert.equal(user.gender, "male");
+        });
+      });
+      it("should return the entries matching {age: {$gte: 30, $lte: 32}}", function () {
+        var thirtyThirtyTwo = users.find({age: {$gte: 30, $lte: 32}});
+        assert.equal(thirtyThirtyTwo.length, 3);
+        thirtyThirtyTwo.forEach(function (user) {
+          assert.equal(user.age >= 30 && user.age <= 32, true);
+        });
+      });
+      it("should return the entries matching {age: {$gt: 32, $lt: 35}}", function () {
+        var thirtyThirtyFive = users.find({age: {$gt: 30, $lt: 35}});
+        assert.equal(thirtyThirtyFive.length, 2);
+        thirtyThirtyFive.forEach(function (user) {
+          assert.equal(user.age > 30 && user.age < 35, true);
+        });
       });
     });
   });
